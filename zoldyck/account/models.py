68947a55061271e7dd72a3d94943252990d2040a
marouanebk <m_boukandoura@estin.dz>
2022-05-26 23:00:39 +0100

    1

--- zoldyck/account/models.py
+++ zoldyck/account/models.py
@@ -44,7 +44,7 @@ class Account(AbstractBaseUser):
 	date_joined				= models.DateTimeField(verbose_name='date joined', auto_now_add=True)
 	last_login				= models.DateTimeField(verbose_name='last login', auto_now=True)
 	is_org					= models.BooleanField(default=False)
-	phone_number			= models.IntegerField(max_length=10 , null=True , blank=True)
+	phone_number			= models.IntegerField(null=True , blank=True)
 	is_admin				= models.BooleanField(default=False)
 	is_active				= models.BooleanField(default=True)
 	is_staff				= models.BooleanField(default=False)
